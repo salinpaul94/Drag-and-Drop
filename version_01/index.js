39d864e0910e8feb4d0c429c5d025b999670ee5d
salinpaul valooran <salinpv@hotmail.com>
2023-11-14 23:06:36 +0530

    saving element that has started dragging

--- version_01/index.js
+++ version_01/index.js
@@ -2,9 +2,8 @@ let lists = document.getElementsByClassName("list");
 let rightBox = document.getElementById("right");
 let leftBox = document.getElementById("left");
 
-
 for (let list of lists) {
-  list.addEventListener("dragstart", function(e) {
-    
-  })
+  list.addEventListener("dragstart", function (e) {
+    let selected = e.target;
+  });
 }
