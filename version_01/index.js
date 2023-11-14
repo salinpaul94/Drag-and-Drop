0a2243f8b0cde699d58cb9f6240dd94c7e7b359b
salinpaul valooran <salinpv@hotmail.com>
2023-11-14 23:04:24 +0530

    adding listerner for drag start event

--- version_01/index.js
+++ version_01/index.js
@@ -1,3 +1,10 @@
 let lists = document.getElementsByClassName("list");
 let rightBox = document.getElementById("right");
 let leftBox = document.getElementById("left");
+
+
+for (let list of lists) {
+  list.addEventListener("dragstart", function(e) {
+    
+  })
+}
